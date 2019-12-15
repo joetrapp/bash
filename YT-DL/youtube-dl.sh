dcc2d980c3d633f5511a68a39794f5604ea50061
joetrapp <53957627+joetrapp@users.noreply.github.com>
2019-12-15 08:56:31 -0800

    Create youtube-dl.sh

---
+++ YT-DL/youtube-dl.sh
@@ -0,0 +1,9 @@
+sudo apt install youtube-dl
+
+sudo youtube-dl --update
+
+sudo youtube-dl -x --audio-format mp3 -a "music.txt" --embed-thumbnail --no-continue --retries 3 --newline
+
+sudo rm -f music.txt
+
+sudo touch music.txt
